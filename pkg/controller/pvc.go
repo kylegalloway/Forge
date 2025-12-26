73a0baefecb7848d87749137ee374680b4fed37a
Kyle Galloway <kyle.galloway.1993@gmail.com>
2025-12-26 13:50:14 -0600

    💀 Vanquished k8s v0.28 EOL ghost, upgraded to v0.35
    Kubernetes v0.28 died in October 2024. Exorcised it from
    go.mod and summoned v0.35 (Dec 2025 release) instead.
    Fixed PVC spec: ResourceRequirements became VolumeResourceRequirements
    in the great API restructuring of v0.35.
    
    Like Van Helsing staking Dracula, EOL dependencies must die.

--- pkg/controller/pvc.go
+++ pkg/controller/pvc.go
@@ -69,7 +69,7 @@ func ensureArtifactPVC(
 			AccessModes: []corev1.PersistentVolumeAccessMode{
 				corev1.ReadWriteOnce,
 			},
-			Resources: corev1.ResourceRequirements{
+			Resources: corev1.VolumeResourceRequirements{
 				Requests: corev1.ResourceList{
 					corev1.ResourceStorage: resource.MustParse(DefaultArtifactStorageSize),
 				},
